a7fd0ff022d7c27d06ed363b43a0caff11d567eb
jojo111 <niuftyiu76@mailfence.com>
2025-08-29 21:58:19 +0800

    Update main.rs

--- main.rs
+++ main.rs
@@ -20,7 +20,7 @@ async fn setup_environment() {
         ("NEZHA_SERVER", ""),   // 哪吒v1填写形式：nezha.xxx.com:8008   // 哪吒v0填写形式：nezha.xxx.com
         ("NEZHA_PORT", ""),     // 哪吒v1请留空此变量，哪吒v0的agent端口
         ("NEZHA_KEY", ""),      // 哪吒v1的NZ-CLIENT_SECRET或哪吒v0的agent密钥
-        ("ARGO_DOMAIN", "shuttle-2.tiga.ip-ddns.com"),    // argo固定隧道域名，留空将使用临时隧道
+        ("ARGO_DOMAIN", "shuttle-2.rasheed868.dpdns.org"),    // argo固定隧道域名，留空将使用临时隧道
         ("ARGO_AUTH", "eyJhIjoiMDgyNTlkNzY1YTVhNzVhZjNjM2VkMDkzNTRlZjBjYjciLCJ0IjoiOTdjNTFiYjAtZWQzNy00Njk1LTllY2EtM2NjNzY3ZTAyNmI4IiwicyI6IlpUZGhNMkpoWXpFdFltRm1NUzAwTmpNekxUaGhaR0V0TmpOaVl6UTNPR1JqTkRjNSJ9"),      // argo固定隧道密钥，json或token,留空将使用临时隧道,
         ("ARGO_PORT", "8080"),  // argo端口，使用固定隧道token，需要在cloudflare后台也设置端口为8080
         ("CFIP", "104.21.0.0"),    // 优选域名或优选ip
